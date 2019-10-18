acbbce475e458e816e407e9f6dc461351710b643
suvidsahay <sahay.suvid61@gmail.com>
2019-10-18 22:52:56 +0530

    scipt to install opencv

---
+++ opencv_installation.sh
@@ -0,0 +1,46 @@
+#! bin/bash
+
+sudo apt-get -y update && sudo apt-get upgrade
+sudo apt-get -y install build-essential cmake pkg-config
+sudo apt-get -y install libjpeg-dev libtiff5-dev libjasper-dev libpng12-dev
+sudo apt-get -y install libavcodec-dev libavformat-dev libswscale-dev libv4l-dev
+sudo apt-get -y install libxvidcore-dev libx264-dev
+sudo apt-get -y install libgtk2.0-dev libgtk-3-dev
+sudo apt-get -y install libatlas-base-dev gfortran
+sudo apt-get -y install python2.7-dev python3-dev
+
+cd ~
+wget -O opencv.zip https://github.com/Itseez/opencv/archive/3.3.0.zip
+unzip opencv.zip
+wget -O opencv_contrib.zip https://github.com/Itseez/opencv_contrib/archive/3.3.0.zip
+unzip opencv_contrib.zip
+wget https://bootstrap.pypa.io/get-pip.py
+sudo python get-pip.py
+sudo python3 get-pip.py
+
+sudo apt install net-tools
+sudo rm -rf ~/.cache/pip
+
+echo -e "\n# virtualenv and virtualenvwrapper" >> ~/.profile
+echo "export WORKON_HOME=$HOME/.virtualenvs" >> ~/.profile
+echo "export VIRTUALENVWRAPPER_PYTHON=/usr/bin/python3" >> ~/.profile
+echo "source /usr/local/bin/virtualenvwrapper.sh" >> ~/.profile
+
+source ~/.profile
+mkvirtualenv cv -p python3
+
+source ~/.profile
+workon cv
+
+pip install numpy
+
+workon cv
+
+cd ~/opencv-3.3.0/
+mkdir build
+cd build
+cmake -D CMAKE_BUILD_TYPE=RELEASE \
+    -D CMAKE_INSTALL_PREFIX=/usr/local \
+    -D INSTALL_PYTHON_EXAMPLES=ON \
+    -D OPENCV_EXTRA_MODULES_PATH=~/opencv_contrib-3.3.0/modules \
+    -D BUILD_EXAMPLES=ON ..
