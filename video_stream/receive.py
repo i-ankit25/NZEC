814f69c896dbdc674dcd9842535442e114ac87fc
RahulKraken <r.rahul4511@gmail.com>
2019-10-19 00:00:30 +0530

    video stream to client : color issue - red too conc

--- video_stream/receive.py
+++ video_stream/receive.py
@@ -1,14 +1,39 @@
 # client to receive stuff from the socket
 
 import socket
+import math
+import numpy as np
+import cv2 as cv
 
 addr_model = ("127.0.0.1", 3000)
+
+# width and height of receiving video
+width = 640
+height = 480
 
 # buffer to receive stuff into
 buff = 512
 
+# num of packets that make up one frame
+# 640 * 480 * 3 = 921600 / 512 = 1800
+num = (640 * 480 * 3) / buff
+
 s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 s.bind(addr_model)
-msg, _ = s.recvfrom(buff)
 
-print(msg)
+while(True):
+  # store chunks of frame
+  data = []
+  
+  for i in range(0, math.floor(num)):
+    msg, _ = s.recvfrom(buff)
+    data.append(msg)
+  
+  string_frame = b''.join(data)
+  frame = np.frombuffer(string_frame, dtype=np.uint8).reshape(height, width, 3)
+  frame = cv.cvtColor(frame, cv.COLOR_BGR2RGB)
+  cv.imshow('receive', frame)
+  if cv.waitKey(25) & 0xFF == ord('q'):
+    cv.destroyAllWindows()
+    break
+
--- video_stream/send.py
+++ video_stream/send.py
@@ -2,6 +2,7 @@
 # send video stream to both server and CV model
 
 import cv2 as cv
+import numpy as np
 import socket
 
 # addresses to stream to
@@ -12,6 +13,7 @@ width = 640
 height = 480
 
 # buffer size for one data packet
+# bcoz otherwise one frame will be huge
 buff = 512
 
 # capture video now
@@ -22,11 +24,16 @@ cap.set(4, width)
 cap.set(3, height)
 
 s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-s.sendto(b'How you doin?', addr_model)
+# s.sendto(b'How you doin?', addr_model)
 
 while(cap.isOpened()):
   ret, frame = cap.read()
   if ret:
+    data = frame.tostring()
+    for i in range(0, len(data), buff):
+      s.sendto(data[i : i + buff], addr_model)
+
+    # s.sendto(data, addr_model)
     cv.imshow("frame", frame)
     if cv.waitKey(25) & 0xFF == ord('q'):
       cv.destroyAllWindows()
