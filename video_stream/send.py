6a13572c43ff53015125550c28ee0ff56db17dae
RahulKraken <r.rahul4511@gmail.com>
2019-10-18 23:10:46 +0530

    sending from server to client - jst msgs

--- video_stream/send.py
+++ video_stream/send.py
@@ -4,14 +4,25 @@
 import cv2 as cv
 import socket
 
+# addresses to stream to
 addr_model = ("127.0.0.1", 3000)
 addr_stream = ("127.0.0.1", 5000)
 
 width = 640
 height = 480
 
+# buffer size for one data packet
+buff = 512
+
 # capture video now
 cap = cv.VideoCapture(0)
+
+# width and height for the video
+cap.set(4, width)
+cap.set(3, height)
+
+s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+s.sendto(b'How you doin?', addr_model)
 
 while(cap.isOpened()):
   ret, frame = cap.read()
