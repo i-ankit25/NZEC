62262bf79e3eec67d901a5b094eb20b8a061dec0
Ankit Gupta <i.ankitgupta25@gmail.com>
2019-10-19 21:39:14 +0530

    mode.py added

--- mode.py
+++ mode.py
@@ -2,7 +2,7 @@ import os
 import subprocess as sp
 import time
 
-extProc1 = sp.Popen(['python','pi_object_detection.py']) 
+extProc1 = sp.Popen(['python','pi-object-detection_new/pi_object_detection.py']) 
 
 os.environ['PREV']='1'
 os.environ['CURRENT']='1'
@@ -15,5 +15,6 @@ while(True):
 
 		elif (os.environ['PREV']=='0' and os.environ['CURRENT']=='1'):
 			sp.Popen.terminate(extProc2)
-			extProc2 = sp.Popen(['python','pi_object_detection.py'])
+			extProc2 = sp.Popen(['python','pi-object-detection_new/pi_object_detection.py'])
 			os.environ['PREV']=1
+		time.sleep(5)
