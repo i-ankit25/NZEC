46edb1d19cc7d7e6275eea2e1a42c25a8d3a03d8
RahulKraken <r.rahul4511@gmail.com>
2019-10-20 02:46:54 +0530

    search and location end points done

--- pyvideostream/main.py
+++ pyvideostream/main.py
@@ -1,7 +1,11 @@
-from flask import Flask, render_template, Response
+from flask import Flask, render_template, Response, request, jsonify
 from camera import VideoCamera
 
+import os
+
 app = Flask(__name__)
+
+locations = []
 
 @app.route('/')
 def index():
@@ -17,6 +21,42 @@ def gen(camera):
 def video_feed():
     return Response(gen(VideoCamera()),
                     mimetype='multipart/x-mixed-replace; boundary=frame')
+
+@app.route('/location', methods=['GET'])
+def getlocations():
+    return jsonify(locations)
+
+@app.route('/location', methods=['POST'])
+def postlocations():
+    if not request.json:
+        return
+    loc = {
+        "lat": request.json['lat'],
+        "lon": request.json['lon']
+    }
+
+    locations.append(loc)
+    print(locations)
+    return jsonify("text", "got it")
+
+@app.route('/search', methods=['POST'])
+def postsearch():
+    if not request.json:
+        return
+    s = request.json['text']
+    print(s)
+    return jsonify("text", "got it")
+
+@app.route('/toggle', methods=['POST'])
+def togglemode():
+    toggleRunMode()
+    return jsonify("var", os.environ['CURRENT'])
+
+def toggleRunMode():
+    if os.environ['CURRENT'] == '1':
+        os.environ['CURRENT'] = '0'
+    else:
+        os.environ['CURRENT'] = '1'
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', debug=True)
