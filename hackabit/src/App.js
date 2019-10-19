ff1375db599b2341edb112c81bdb25d6449a348a
shivansh2598 <shivanshsinha2598@gmail.com>
2019-10-20 03:06:59 +0530

    website update

--- hackabit/src/App.js
+++ hackabit/src/App.js
@@ -4,9 +4,11 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-     <h1>Video Streaming Demonstration</h1>
-     <br/>
-     <img id="bg" src="http://localhost:5000/video_feed" width="800" height="600"></img>
+     <h1 style = {{'font-family': 'Lobster, cursive', 'fontSize': '40px'}}>Video Streaming Demonstration !!</h1>
+     <h2 style = {{'fontSize':'30px'}}>Find your loved ones, anytime, anywhere !!</h2>
+     <img style={{'border': '1px solid',
+    'padding': '10px',
+    'box-shadow' : '7px 12px 10px #888888'}} id="bg" src="http://localhost:5000/video_feed" width="800" height="600"></img>
     </div>
   );
 }
