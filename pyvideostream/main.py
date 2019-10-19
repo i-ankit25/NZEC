f64337b1180e4cd40e8081bb444b8b26cfc51969
RahulKraken <r.rahul4511@gmail.com>
2019-10-20 03:05:55 +0530

    class parsing and dump to file

--- pyvideostream/main.py
+++ pyvideostream/main.py
@@ -7,6 +7,11 @@ app = Flask(__name__)
 
 locations = []
 
+CLASSES = ["background", "aeroplane", "bicycle", "bird", "boat",
+	"bottle", "bus", "car", "cat", "chair", "cow", "diningtable",
+	"dog", "horse", "motorbike", "person", "pottedplant", "sheep",
+	"sofa", "train", "tvmonitor"]
+
 @app.route('/')
 def index():
     return render_template('index.html')
@@ -45,8 +50,14 @@ def postsearch():
         return
     s = request.json['text']
     print(s)
-    file = open('../search.txt', 'w')
-    file.write(s)
+    keys = s.split(' ')
+    for key in keys:
+        if CLASSES.__contains__(key):
+            print('writing', key, 'to file')
+            file = open('../search.txt', 'r+')
+            file.truncate(0)
+            file.write(key)
+            break
     return jsonify("text", "got it")
 
 @app.route('/toggle', methods=['POST'])
