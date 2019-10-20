1d2aaa0203160719b38c500fad6d4f4dcca26f83
RahulKraken <r.rahul4511@gmail.com>
2019-10-20 09:37:23 +0530

    bootstrap removed

--- hackabit/src/App.js
+++ hackabit/src/App.js
@@ -8,7 +8,7 @@ function App() {
      <h2 style = {{'fontSize':'30px'}}>Find your loved ones, anytime, anywhere !!</h2>
      <img style={{'border': '1px solid',
     'padding': '10px',
-    'box-shadow' : '7px 12px 10px #888888'}} id="bg" src="http://localhost:5000/video_feed" width="800" height="600"></img>
+    'box-shadow' : '7px 12px 10px #888888'}} id="bg" src="http://10.42.0.186:5000/video_feed" width="800" height="600"></img>
     </div>
   );
 }
